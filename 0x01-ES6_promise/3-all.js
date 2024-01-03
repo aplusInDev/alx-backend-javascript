b0690dfc9f2c18951ad1962bfba9d57ee813ecd8
aplusJsDev <laabid.abdessamadplus@gmail.com>
2024-01-03 22:24:55 +0100

    fixing task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function handleProfileSignup() {
-  const values = await Promise.all([uploadPhoto(), createUser()]);
-  console.log(values[0].body, values[1].firstName, values[1].lastName);
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(values[0].body, values[1].firstName, values[1].lastName);
+    });
 }
