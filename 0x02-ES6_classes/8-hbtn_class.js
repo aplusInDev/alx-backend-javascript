cae33c82b7705c2c5c80ab82d4dc05897817c112
aplusJsDev <laabid.abdessamadplus@gmail.com>
2024-01-04 22:01:20 +0100

    all done

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,30 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  toString() {
+    return this._location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(newSize) {
+    this._size = newSize;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(newLocation) {
+    this._location = newLocation;
+  }
+}
